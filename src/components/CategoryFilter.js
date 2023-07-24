30db98f8e2cb5a5069b013ae2cbf19df49b0acc0
Briankipr <brian.kiprono1@student.moringaschool.com>
2023-07-24 08:21:26 +0300

    assignment done

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,33 @@
+// import React from "react";
+
+// function CategoryFilter() {
+//   return (
+//     <div className="categories">
+//       <h5>Category filters</h5>
+//       {/* render <button> elements for each category here */}
+//     </div>
+//   );
+// }
+
+// export default CategoryFilter;
+
+
+
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({ categories, selectedCategory, onCategoryFilter }) {
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {categories.map((category) => (
+        <button
+          key={category}
+          className={selectedCategory === category ? "selected" : ""}
+          onClick={() => onCategoryFilter(category)}
+        >
+          {category}
+        </button>
+      ))}
     </div>
   );
 }
--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,16 +1,66 @@
-import React from "react";
+// import React from "react";
 
-function NewTaskForm() {
+// function NewTaskForm() {
+//   return (
+//     <form className="new-task-form">
+//       <label>
+//         Details
+//         <input type="text" name="text" />
+//       </label>
+//       <label>
+//         Category
+//         <select name="category">
+//           {/* render <option> elements for each category here */}
+//         </select>
+//       </label>
+//       <input type="submit" value="Add task" />
+//     </form>
+//   );
+// }
+
+// export default NewTaskForm;
+
+
+import React, { useState } from "react";
+
+function NewTaskForm({ categories, onTaskFormSubmit }) {
+  const [text, setText] = useState("");
+  const [category, setCategory] = useState("Code");
+
+  function handleTextChange(event) {
+    setText(event.target.value);
+  }
+
+  function handleCategoryChange(event) {
+    setCategory(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault(); // Prevent form submission
+    const newTask = {
+      id: Date.now(),
+      text: text,
+      category: category,
+    };
+    onTaskFormSubmit(newTask);
+    setText("");
+    setCategory("Code");
+  }
+
   return (
-    <form className="new-task-form">
+    <form className="new-task-form" onSubmit={handleSubmit}>
       <label>
         Details
-        <input type="text" name="text" />
+        <input type="text" name="text" value={text} onChange={handleTextChange} />
       </label>
       <label>
         Category
-        <select name="category">
-          {/* render <option> elements for each category here */}
+        <select name="category" value={category} onChange={handleCategoryChange}>
+          {categories.map((category) => (
+            <option key={category} value={category}>
+              {category}
+            </option>
+          ))}
         </select>
       </label>
       <input type="submit" value="Add task" />
